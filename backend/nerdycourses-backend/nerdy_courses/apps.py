12c0d6bcd20b6a6875f5d351555d6449ed4b7a72
rohit-b04 <rohitbargal0@gmail.com>
2025-11-21 18:40:44 +0530

    added gitignore

--- backend/nerdycourses-backend/nerdy_courses/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NerdyCoursesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nerdy_courses'
--- backend/nerdycourses-backend/nerdy_courses/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("api/register", views.register),
-    path("api/login", views.login),
-    path("api/logout", views.logout),
-    path("api/search-courses", views.search_courses)
-]
